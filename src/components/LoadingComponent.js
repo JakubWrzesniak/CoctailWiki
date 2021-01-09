51614e213f79387fceda0eb7bc8abd5df876a8ce
Jakub Wrześniak <jakubwrzesniak@macbook-pro-jakub.home>
2021-01-09 14:42:03 +0100

    Change loading component, chnage background colour and card color

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -4,10 +4,16 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export const Loading = () =>{
   return(
-    <div className="col-12">
-        <FontAwesomeIcon pulse = {true}  icon ={faSpinner} color="black"/>
-        <span className="fa fa-spinner fa-pulse fa-3x fa-fw test-primary"></span>
-        <p>Loading . . .</p>
+    <div className = "container">
+      <div className ="row">
+        <div className="col-3 offset-5">
+          <div className = "loading">
+            <FontAwesomeIcon pulse = {true}  icon ={faSpinner} color="black" size="7x"/>
+            <span className="fa fa-spinner fa-pulse fa-3x fa-fw test-primary"></span>
+            <p>Loading . . .</p>
+          </div>
+        </div>
+        </div>
     </div>
   );
 };
